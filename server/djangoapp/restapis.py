bc1fc73b3fa17e0bf01d0a420be9bab155bf0026
Christoph9369 <mojuyefelix@gmail.com>
2024-11-04 16:23:24 -0500

    format restapis

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,33 +1,41 @@
-import requests # type: ignore
+import requests
 import os
-from dotenv import load_dotenv # type: ignore
+from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv("backend_url", default="http://localhost:3030")
+backend_url = os.getenv(
+    'backend_url', default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
-    "sentiment_analyzer_url", default="http://localhost:5050/"
-)
+    'sentiment_analyzer_url',
+    default="http://localhost:5050/")
 
+
+# def get_request(endpoint, **kwargs):
+# Add code for get requests to back end
 def get_request(endpoint, **kwargs):
     params = ""
-    if kwargs:
+    if (kwargs):
         for key, value in kwargs.items():
             params = params + key + "=" + value + "&"
 
-    request_url = backend_url + endpoint + "?" + params
+    request_url = backend_url+endpoint+"?"+params
 
     print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as err:
         # If any error occurs
-        print("Network exception occurred")
+        print(f"Network exception occurred {err}")
 
+
+# def analyze_review_sentiments(text):
+# request_url = sentiment_analyzer_url+"analyze/"+text
+# Add code for retrieving sentiments
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url + "analyze/" + text
+    request_url = sentiment_analyzer_url+"analyze/"+text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
@@ -36,6 +44,9 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
+# def post_review(data_dict):
+# Add code for posting review
 def post_review(data_dict):
     request_url = backend_url + "/insert_review"
     try:
