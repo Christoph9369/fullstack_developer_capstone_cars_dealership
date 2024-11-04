af2690d02e88e35e79a85cb9ec0893c666e547ef
Christoph9369 <mojuyefelix@gmail.com>
2024-11-04 16:36:54 -0500

    format models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -24,8 +24,10 @@ class CarModel(models.Model):
     type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
 
     year = models.IntegerField(
-        default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)]
+        default=2023, validators=[MaxValueValidator(2023), 
+        MinValueValidator(2015)]
     )
 
     def __str__(self):
-        return f"{self.car_make.name} {self.name}"  # Return the car make and model as the string representation
+        return f"{self.car_make.name} {self.name}" 
+
